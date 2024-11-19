b1679201f2e1252e4c5b40fd1936e9b9ce0a21f9
k2wing <serna_ro@hotmail.com>
2024-11-20 03:31:31 +0900

    update: ブックマークレットを外部ファイル化して読み込みやすいように機能修正
    不具合修正や機能追加等メンテナンスをしやするくすため。

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,34 +1,18 @@
-(function(_={
-		/*
-		// パラメータ一覧
-		'氏名': '-----',
-		'タイミング': '',
-		'Eメール1': 'hoge@example.com',
-		'Eメール2': 'foo@example.com',
-		'出発予定時間': '0000',
-		'到着予定時間': '0000',
-		'開始予定時間': '0000',
-		'検温': '36.0',
-		'開始時間': '0000',
-		'終了時間': '0000',
-		'休憩時間': '0000',
-		*/
-		__PARAMS__ // 置換用パラメータ変数
-	}) {
-	function getConts(id) {
+(function() {
+	function getSelectContents(id) {
 		return {
 			// selectの入力項目はラベルの次の次の要素 <div class='tempContDiv'>
-			s: document.getElementById(id).nextElementSibling.nextElementSibling.getElementsByClassName(
-				'select2-selection__rendered'),// 表示用要素は <span>
-			h: document.getElementById(id).nextElementSibling.nextElementSibling.getElementsByClassName(
-				'select2-hidden-accessible')// 送信用要素(隠し要素)は <select>
+			select: document.getElementById(id).nextElementSibling.nextElementSibling.getElementsByClassName(
+				'select2-hidden-accessible'),// 送信用要素(隠し要素)は <select>
+			span: document.getElementById(id).nextElementSibling.nextElementSibling.getElementsByClassName(
+				'select2-selection__rendered')// 表示用要素は <span>
 		}
 	}
-	function setSelect(id, v, i=0, r=true) { // id、vは値、iは一つの項目に複数の入力枠がある場合のインデックスで基本は0で時間系なら0=時間/1=分、rはtrueでグレーアウト(クラス)の削除
-		const c=getConts(id);
-		c.s[i].title = c.s[i].children[0].textContent = v; // <span> 表示用として親のtitile属性、子のテキストに値が設定される
-		if(r) c.s[i].children[0].classList.remove('drpDwnPlaceholder'); // タイミングと検温で「-select-」(初期)時はグレーアウトしているので通常表示にするために削除
-		c.h[i].querySelector(`option[value='${v}']`).selected = true; // <select>フォームで送信するデータの設定
+	function setSelect(id, value, i=0, isRemoveGrayoutClass=true) { // id、vは値、iは一つの項目に複数の入力枠がある場合のインデックスで基本は0で時間系なら0=時間/1=分、rはtrueでグレーアウト(クラス)の削除
+		const selectContents = getSelectContents(id);
+		selectContents.span[i].title = selectContents.span[i].children[0].textContent = value; // <span> 表示用として親のtitile属性、子のテキストに値が設定される
+		if(isRemoveGrayoutClass) selectContents.span[i].children[0].classList.remove('drpDwnPlaceholder'); // タイミングとtemperatureCheckで「-select-」(初期)時はグレーアウトしているので通常表示にするために削除
+		selectContents.select[i].querySelector(`option[value='${v}']`).selected = true; // <select>フォームで送信するデータの設定
 	}
 	function setTime(id, hhmm) {
 		setSelect(id, hhmm.substring(0,2), 0, false);
@@ -44,7 +28,7 @@
 		
 		// タイミングの指定で表示する項目と隠す項目を変更、表示/非常時で入力必須フラグも変更
 		items.forEach(id=>{
-			let e=document.getElementById(id);
+			let e = document.getElementById(id);
 			// 表示する項目を文字列可してその中に指定のID文字列が存在する場合は表示、存在し無い場合は非表示で振り分け
 			if(s.indexOf(id) == -1) {
 				e.style.display='none';
@@ -55,23 +39,43 @@
 			}
 		});
 	}
-		
-	if(!!_['タイミング']) {//プロパティ未定義、空文字のチェック
-		setSelect('Dropdown-arialabel-cont',_['タイミング']);
+	
+	/*
+		// パラメータ一覧
+		name: '-----', 					//氏名
+		timing: '', 					//タイミング
+		email: 'hoge@example.com', 		//Eメール1
+		email1: 'foo@example.com', 		//Eメール2
+		scheduledDepartureTime: '0000', //出発予定時間
+		estimatedArrivalTimes: '0000', 	//到着予定時間
+		scheduledStartTime: '0000', 	//開始予定時間
+		temperatureCheck: '36.0', 		//検温結果
+		startTime: '0000', 				//開始時間
+		endTime: '0000', 				//終了時間
+		breakTime: '0000', 				//休憩時間
+	*/
+	// コードのメンテナンス性を考えて利用者側からはこのスクリプトを外部サーバーから読み込むようにする。
+	// データの引き渡しを引数にすると無名関数ではなくなり関数名の衝突が発生する事も考えられるため従来の方法(即時)で関数を実行する。
+	// データの引き渡しはセッションストレージに一時的にデータを保存しここで取り出す。
+	// セッションストレージのキーはGUIDにしてブックマークレットを実行するページ内のキーと衝突しないようにする。
+	const params = Object.freeze(JSON.parse(sessionStorage.getItem('ebde39226ae24807bee2567dc4c19c8e')));
+
+	if(!!params.timing) {//プロパティ未定義、空文字の場合はフォームに入力しない
+		setSelect('Dropdown-arialabel-cont',_.timing);
 		const showItemsTbl = {
 			'前日確認':()=>{hideAndShowItems(['Time-li','Time1-li','Time2-li']);},
-			'出発':()=>{hideAndShowItems(['Dropdown1-li','Dropdown2-li','Time3-li'].concat(getConts('Dropdown2-arialabel-cont').h[0].selectedOptions[0].value=='問題あり'?['MultiLine-li']:[]));},
+			'出発':()=>{hideAndShowItems(['Dropdown1-li','Dropdown2-li','Time3-li'].concat(getSelectContents('Dropdown2-arialabel-cont').select[0].selectedOptions[0].value=='問題あり'?['MultiLine-li']:[]));},
 			'到着':()=>{hideAndShowItems(['Time4-li']);},
 			'入館':()=>{hideAndShowItems(['']);},
 			'退館':()=>{hideAndShowItems(['Time5-li','Time6-li','Time7-li']);},
 		};
-		showItemsTbl[_['タイミング']]?.();
+		showItemsTbl[_.timing]?.();
 	}
-	if(!!_['氏名']) setInput('SingleLine1-arialabel', _['氏名']);
-	if(!!_['Eメール1']) setInput('Email-arialabel', _['Eメール1']);
-	if(!!_['Eメール2']) setInput('Email1-arialabel', _['Eメール2']);
-	if(!!_['出発予定時間']) {
-		setTime('Time-arialabel', _['出発予定時間']);
+	if(!!params.name) setInput('SingleLine1-arialabel', params.name);
+	if(!!params.email) setInput('Email-arialabel', params.email);
+	if(!!params.email1) setInput('Email1-arialabel', params.email1);
+	if(!!params.scheduledDepartureTime) {
+		setTime('Time-arialabel', params.scheduledDepartureTime);
 		// 前日確認時の明日の日付
 		document.getElementById('Date-date').value = new Date(new Date().setDate(new Date()
 			.getDate() + 1)).toLocaleDateString('ja-JP', {
@@ -80,13 +84,13 @@
 			day: '2-digit'
 		});
 	}
-	if(!!_['到着予定時間']) setTime('Time1-arialabel', _['到着予定時間']);
-	if(!!_['開始予定時間']) setTime('Time2-arialabel', _['開始予定時間']);
+	if(!!params.estimatedArrivalTimes) setTime('Time1-arialabel', params.estimatedArrivalTimes);
+	if(!!params.scheduledStartTime) setTime('Time2-arialabel', params.scheduledStartTime);
 	
-	if(!!_['検温']) setSelect('Dropdown1-arialabel-cont',_['検温']);
+	if(!!params.temperatureCheck) setSelect('Dropdown1-arialabel-cont', params.temperatureCheck);
 	
-	if(!!_['開始時間']) setTime('Time5-arialabel', _['開始時間']);
-	if(!!_['終了時間']) setTime('Time6-arialabel', _['終了時間']);
-	if(!!_['休憩時間']) setTime('Time7-arialabel', _['休憩時間']);
+	if(!!params.startTime) setTime('Time5-arialabel', params.startTime);
+	if(!!params.endTime) setTime('Time6-arialabel', params.endTime);
+	if(!!params.breakTime) setTime('Time7-arialabel', params.breakTime);
 })()
 
